f68450932eb0e9bf8e0b011d4f536c63f4678ac8
Sarsharses <ljotte93@gmail.com>
2024-01-15 20:35:55 +0100

    Functies moeten lowercase namen hebben

--- dobbelroller.py
+++ dobbelroller.py
@@ -12,58 +12,60 @@ import random
 
 vorigeRol = []
 
-def dobbelInput():
+
+def dobbelinput():
     global dobbelsteen
     dobbelsteen = input("Welke dobbelsteen wil je rollen? d")
-    
+
     if dobbelsteen.isdigit():
         dobbelsteen = int(dobbelsteen)
     else:
         print("Foutieve input")
-        dobbelInput()
-     
-def dobbelRol():
+        dobbelinput()
 
-    #uitkomst is een willekeurig getal tussen 1 en de opgegeven dobbelsteen
+
+def dobbelrol():
+    # uitkomst is een willekeurig getal tussen 1 en de opgegeven dobbelsteen
     global uitkomst
-    uitkomst = int(random.randrange(1, dobbelsteen+1))
+    uitkomst = int(random.randrange(1, dobbelsteen + 1))
     print(" Je rolde met een d" + str(dobbelsteen))
-    print(" En je gooide een", uitkomst) 
+    print(" En je gooide een", uitkomst)
     playsound('bell.wav')
-    
-    #Als de rol maximaal of 1 is wordt dit extra benadrukt
+
+    # Als de rol maximaal of 1 is wordt dit extra benadrukt
     if uitkomst == dobbelsteen:
         print("Je hebt een perfecte rol!")
     if uitkomst == 1:
         print("Je rol is gefaald!")
-        
-    #De 5 laatste uitkomsten worden opgeslagen in een lijst
+
+    # De 5 laatste uitkomsten worden opgeslagen in een lijst
     vorigeRol.insert(0, uitkomst)
     if len(vorigeRol) > 5:
         vorigeRol.pop()
-        
+
     print("De laatste worpen waren:")
     print(vorigeRol)
-    opnieuwRollen()
-   
-def opnieuwRollen():
-    global opnieuw 
+    opnieuwrollen()
+
+
+def opnieuwrollen():
+    global opnieuw
     opnieuw = input("\nWil je nogmaals rollen? Y/N\nEen leeg antwoord wordt als Ja gezien. ")
 
     if opnieuw.lower() == "y":
-        dobbelInput()
-        dobbelRol()
+        dobbelinput()
+        dobbelrol()
     elif opnieuw.lower() == "n":
         exit()
     elif opnieuw == "":
-        dobbelInput()
-        dobbelRol()
+        dobbelinput()
+        dobbelrol()
     else:
         print("Foutieve input")
-        opnieuwRollen()
+        opnieuwrollen()
 
-    
-print("Welkom bij de ultra-fantastische mega-ultieme dobbelsteenrolervaring van 2023!!!1!\n")   
 
-dobbelInput()
-dobbelRol()
+print("Welkom bij de ultra-fantastische mega-ultieme dobbelsteenrolervaring van 2023!!!1!\n")
+
+dobbelinput()
+dobbelrol()
