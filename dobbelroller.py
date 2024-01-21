be4172e91db9da20f8ff036c98a5def0e824060f
Sarsharses <LJOtte93@gmail.com>
2024-01-21 15:08:26 +0100

    Translated to English and made progress on GUI

--- dobbelroller.py
+++ dobbelroller.py
@@ -14,10 +14,10 @@ import argparse
 
 customtkinter.set_default_color_theme("dark-blue")
 customtkinter.set_appearance_mode("dark")
+text_color = "#FFFFFF"
 
-# Stelt de window van de applicatie in
-class FrameLinks(customtkinter.CTkFrame):
-    text_color = "#FFFFFF"
+# Sets up the left frame
+class FrameLeft(customtkinter.CTkFrame):
 
     def __init__(self, master):
         super().__init__(master)
@@ -25,20 +25,36 @@ class FrameLinks(customtkinter.CTkFrame):
         self.grid_rowconfigure((0, 2), weight = 1)
         #self.grid_columnconfigure(0, weight = 1)
 
-        # Dobbelsteenlabel
-        self.label = customtkinter.CTkLabel(self, text = "Kies een aantal ogen", fg_color="transparent", text_color = self.text_color)
-        self.label.grid(row = 0, column = 0, padx = 20, pady = 20, sticky="new")
+        # Dicelabel
+        self.label = customtkinter.CTkLabel(self, text = "Choose your dice", fg_color="transparent", text_color = text_color)
+        self.label.grid(row = 0, column = 0, columnspan = 2, padx = 20, pady = 20, sticky="NEW")
 
-        # Checkboxes voor standaard stenen [4, 6, 8, 10, 12, 20]
+        # Checkboxes for standard dice [4, 6, 8, 10, 12, 20]
 
-        # Custom dobbelsteen
-        self.l_entry = customtkinter.CTkLabel(self, text = "Anders:")
-        self.entry = customtkinter.CTkEntry(self, placeholder_text = "Hoeveel ogen?")
-        self.entry.grid(row = 2, column = 0, padx = 20, pady = 20, sticky="sew")
+        # Custom dice entrybox
+        self.l_entry = customtkinter.CTkLabel(self, text = "Custom:")
+        self.entry = customtkinter.CTkEntry(self, placeholder_text = "How many eyes?")
+        self.entry.grid(row = 2, column = 0, columnspan = 2, padx = 20, pady = 20, sticky="SEW")
+        
+
+class FrameMid(customtkinter.CTkFrame):
+    
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.grid_rowconfigure((0), weight = 1)
+        self.grid_columnconfigure(1, weight = 1)
+        
+        # Rollbutton
+        self.button = customtkinter.CTkButton(self, text = "Roll!", command = self.button_click)
+        self.button.grid(row = 2, column = 1, padx = 10, pady = 20, sticky="EW")
+    
+    
+    def button_click(self):
+        dice_roll(6)
 
 
-class FrameRechts(customtkinter.CTkFrame):
-    text_color = "#FFFFFF"
+class FrameRight(customtkinter.CTkFrame):
 
     def __init__(self, master):
         super().__init__(master)
@@ -46,12 +62,11 @@ class FrameRechts(customtkinter.CTkFrame):
         self.grid_rowconfigure((0, 1), weight = 1)
         self.grid_columnconfigure(0, weight = 1)
         
-        # Laatste rollen
-        self.laatste_rol = customtkinter.CTkLabel(self, text = f"Laatste worpen:", fg_color="transparent", text_color = self.text_color)
-        self.laatste_rol.grid(row = 0, column = 0, padx = 20, pady = 20, sticky="new")
-        self.rol_hist = customtkinter.CTkLabel(self, text = f"{vorigeRol}", fg_color="transparent", text_color = self.text_color)
-        self.rol_hist.grid(row = 1, column = 0, padx = 20, pady = 20, sticky="ew")
-
+        # Last rolls
+        self.last_roll_label = customtkinter.CTkLabel(self, text = f"Last rolls:", fg_color="transparent", text_color = text_color)
+        self.last_roll_label.grid(row = 0, column = 0, padx = 40, pady = 20, sticky="NEW")
+        self.roll_hist = customtkinter.CTkLabel(self, text = f"{last_roll}", fg_color="transparent", text_color = text_color)
+        self.roll_hist.grid(row = 1, column = 0, padx = 20, pady = 20, sticky="EW")
   
 
 class App(customtkinter.CTk):
@@ -59,37 +74,33 @@ class App(customtkinter.CTk):
 
     def __init__(self):
         super().__init__()
-        self.geometry("500x300")
-        self.title("Dobbelroller v0.1")
-        #self.resizable(width=False, height=False)
+        self.geometry("600x300")
+        self.title("Diceroller v0.1")
+        self.resizable(width=False, height=False)
 
-        # Maak een 3x3 grid
-        self.grid_rowconfigure((0, 2), weight = 1)
-        self.grid_columnconfigure((0, 2), weight = 1)
+        # Make a 3x3 grid
+        self.grid_rowconfigure((1), weight = 1)
+        self.grid_columnconfigure((0, 1, 2), weight = 1)
 
-        # Frame links
-        self.dobbel_frame = FrameLinks(self)
-        self.dobbel_frame.grid(row = 0, column = 0, rowspan = 3, padx = 10, pady = 10, sticky = "nsw")
+        # Frame left
+        self.dobbel_frame = FrameLeft(self)
+        self.dobbel_frame.grid(row = 0, column = 0, rowspan = 3, padx = 10, pady = 10, sticky = "NSW")
 
-        # Frame rechts
-        self.hist_frame = FrameRechts(self)
-        self.hist_frame.grid(row = 0, column = 2, rowspan = 3, padx = 10, pady = 10, sticky = "nsw")
+        # Frame right
+        self.hist_frame = FrameRight(self)
+        self.hist_frame.grid(row = 0, column = 2, rowspan = 3, padx = 10, pady = 10, sticky = "NSE")
         
-        # Rolknop
-        self.button = customtkinter.CTkButton(self, text = "Rol!", command = self.button_click)
-        self.button.grid(row = 2, column = 1, padx = 20, pady = 10, sticky="ew")
+        # Frame mid
+        self.mid_frame = FrameMid(self)
+        self.mid_frame.grid(row = 0, column = 1, rowspan = 3, pady = 10, sticky = "NSEW")
 
-    def button_click(self):
-        dobbelrol(6)
-
-
-# Kijkt naar de command line arguments om te zien of de GUI wordt aangeroepen
+# Check for commandline arg to run without GUI
 parser = argparse.ArgumentParser()
 parser.add_argument("-c", "--CLI", action = 'store_true', help = "Show CLI only")
 args = parser.parse_args()
 
-# Creeert de list om de laatste 5 rollen bij te houden
-vorigeRol = []
+# Creates list to keep track of last throws
+last_roll = []
 
 def main():
     if args.CLI:
@@ -98,81 +109,80 @@ def main():
         graphic_interface()
 
 
-# Teken de dobbelroller op het scherm
+# Draw the GUI
 def graphic_interface():
     app = App()
     app.mainloop()
 
 
-# Runt de CLI versie
+# Runs without GUI
 def command_interface():
-    print("Welkom bij de ultra-fantastische mega-ultieme dobbelsteenrolervaring van 2023!!!1!\n")
-    dobbelrol(dobbelinput())
+    print("Welcome to the ultra-fantastic mega-ultimate dicerolling experience of 2024!!!1!\n")
+    dice_roll(dice_input())
  
-# Vraag om een integer en kijk of het daadwerkelijk een getal is
-# Wordt alleen in de CLI gebruikt
+# Asks for an int until an int has been given, only runs in CLI
 def get_int(prompt):
     while True:
         try:
             return int(input(prompt))
         except ValueError:
-            print("Foutieve input")
+            print("Wrong input")
 
 
-# Stelt in de CLI in welke dobbelsteen er gerold wordt
-def dobbelinput():
-    dobbelsteen = 0
-    while dobbelsteen < 1:
+# Checks which dice the user wants to use
+def dice_input():
+    dice = 0
+    while dice < 1:
         # TODO Er moet nog een Else komen voor de GUI
         if (args.CLI == True):
-            dobbelsteen = get_int("Welke dobbelsteen wil je rollen? d")
+            dice = get_int("Which dice would you like to roll? d")
         # TODO Else StringVar() iets
-    return dobbelsteen
+    return dice
 
 
-# Rolt met de dobbelsteen
-def dobbelrol(d):
-    # uitkomst is een willekeurig getal tussen 1 en de opgegeven dobbelsteen
-    uitkomst = int(random.randrange(1, d + 1))
+# Rolls the dice
+def dice_roll(d):
+    # result is a random integer between 1 and the given dice
+    result = int(random.randrange(1, d + 1))
     # TODO Er moet nog een Else komen voor de GUI
-    print(" Je rolde met een d" + str(d))
-    print(" En je gooide een", uitkomst)
+    print(f" You threw a d{str(d)}")
+    print(f" And rolled a {result}")
 
-    # Als de rol maximaal of 1 is wordt dit extra benadrukt
-    if uitkomst == d:
-        print("Je hebt een perfecte rol!")
+    # Plays special sounds for a perfect or failed roll
+    if result == d:
+        print("That's a perfect throw!")
         playsound('sounds/perfect.wav')
-    elif uitkomst == 1:
-        print("Je rol is gefaald!")
+    elif result == 1:
+        print("Your throw has failed!")
         playsound('sounds/failure.wav')
     else:    
         playsound('sounds/bell.wav')
 
-    # De 5 laatste uitkomsten worden opgeslagen in een lijst
-    vorigeRol.insert(0, uitkomst)
-    if len(vorigeRol) > 5:
-        vorigeRol.pop()
+    # Save last 5 throws
+    last_roll.insert(0, result)
+    if len(last_roll) > 5:
+        last_roll.pop()
 
     
-    print("De laatste worpen waren:")
-    print(vorigeRol)
+    print("Your last rolls are:")
+    print(last_roll)
     print()
     if (args.CLI == True):
-        opnieuwrollen()
+        roll_again()
 
 
-def opnieuwrollen():
-    msg = "Wil je nogmaals rollen? Y/n "
-    opnieuw = input(msg)
+def roll_again():
+    msg = "Would you like to roll again? Y/n "
+    again = input(msg)
 
     while True:
-            if (opnieuw.lower() == "y" or opnieuw == ""): 
-                dobbelrol(dobbelinput())
-            elif (opnieuw.lower() == "n"):
+            if (again.lower() == "y" or again == ""): 
+                dice_roll(dice_input())
+            elif (again.lower() == "n"):
                 exit()
             else:
-                print("\nFoutieve input")
-                opnieuw = input(msg)
+                print("\nWrong input")
+                again = input(msg)
 
 
 main()
